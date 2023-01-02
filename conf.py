32028644ca87265a7d6fc0b83483cd8e764c7106
MKadirr <108466711+MKadirr@users.noreply.github.com>
2023-01-02 19:19:09 +0100

    Add files via upload

--- conf.py
+++ conf.py
@@ -7,12 +7,17 @@ s = "mv $HOME/afs/.confs/config/i3/config $HOME/afs/.confs/config/i3/" + tmp
 os.system(s)
 print("saved as " + tmp + "\n")
 
+with open('log', 'a') as fichier:
+    fichier.write("config saved as " + tmp)
+
+
+
 print("Changing i3 confing : ", end = "")
 os.system('cp myconfig $HOME/afs/.confs/config/i3/config')
 print("Done\n")
 
 print("Images : ", end = "")
-os.system('cp -r images $HOME/afs/.confs/')
+os.system('mv -r .images $HOME/afs/.confs/')
 print("Done\n")
 
 print("\nAll Done, enjoy")
